2baf238e23e96ffb6daaf468c78a2ba4ddd661e7
gyb333 <876368840@qq.com>
2020-09-08 13:51:14 +0800

    2020年9月8日13:51:08

---
+++ AdvancedResearch/locks/blockingKeyCountLimit.go
@@ -0,0 +1,59 @@
+package locks
+
+import (
+	"sync"
+)
+
+type blockingKeyCountLimit struct {
+	sync.RWMutex
+	current map[string]*semaphore
+	limit int
+}
+
+func NewBlockingKeyCountLimit(n int) *blockingKeyCountLimit{
+	return &blockingKeyCountLimit{
+		current: make(map[string]*semaphore),
+		limit: n,
+	}
+}
+
+func (bl *blockingKeyCountLimit) ValueSum() int{
+	bl.RLock()
+	defer bl.RUnlock()
+	all:=0
+	for _,v:=range bl.current{
+		all+=v.Value()
+	}
+	return all
+}
+
+func (bl *blockingKeyCountLimit) Acquire(key []byte) {
+	strKey:=string(key)
+	bl.Lock()
+	k,ok:=bl.current[strKey];
+	if !ok{
+		k=NewSemaphore(bl.limit)
+		bl.current[strKey]=k
+	}
+	k.refs++
+	bl.Unlock()
+	k.Acquire()
+}
+
+func (bl *blockingKeyCountLimit) Release(key []byte)  {
+	strKey :=string(key)
+	bl.Lock()
+	k,ok:=bl.current[strKey]
+	if !ok{
+		panic("key not in map. Possible reason: Release without Acquire.")
+	}
+	k.refs--
+	if k.refs<0{
+		panic("internal error: refs < 0")
+	}
+	if k.refs==0{
+		delete(bl.current,strKey)
+	}
+	bl.Unlock()
+	k.Release()
+}
---
+++ AdvancedResearch/locks/semaphore.go
@@ -0,0 +1,47 @@
+package locks
+
+import "sync"
+
+type semaphore struct {
+	refs int
+
+	max int
+	value int
+	sync.Cond
+}
+
+func NewSemaphore(max int) *semaphore  {
+	return &semaphore{
+		max: max,
+		Cond: sync.Cond{L:new(sync.Mutex)},
+	}
+}
+
+func (s *semaphore) Value() int {
+	s.L.Lock()
+	defer s.L.Unlock()
+	return s.value
+}
+
+func (s *semaphore) Acquire()  {
+	s.L.Lock()
+	defer s.L.Unlock()
+	for{
+		if s.value+1<=s.max{
+			s.value++
+			return
+		}
+		s.Wait()
+	}
+	panic("unexpected branch")
+}
+
+func (s *semaphore) Release(){
+	s.L.Lock()
+	defer s.L.Unlock()
+	s.value--
+	if s.value<0{
+		panic("semaphore Release without Acquire")
+	}
+	s.Signal()
+}
